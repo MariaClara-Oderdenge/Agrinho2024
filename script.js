a6a19d95e3a1e2dad6a9b711b1798c7a22b677b3
Maria Clara ☆ <104856731+MariaClara-Oderdenge@users.noreply.github.com>
2024-05-17 11:05:21 -0300

    .

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function SIM (){
 }
 function desvia(t){
     var bnt - t;
-    bnt.style.position
+    bnt.style.position-"absolute";
+    bnt.style.boton-geraPosição(10, 90);
+    bnt.style.left-geraPosição(10, 90);
+    console.log(Opa, desviei)
+}
+
+function geraPosição(min, max) {
+    return Math.random()  (max - min) + "%";
 }
